e30ddc8c427c85b5dc033ac354cffe0363e8704b
Jared <jared@tagtoo.com>
2019-10-29 17:55:39 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Input from "@material-ui/core/Input";
 
+// \u4e0a\u6d77
 function App() {
+  const [value, setValue] = useState("");
+  const [decodeValue, setDecodeValue] = useState("");
+
+  const unicodeToChar = text => {
+    return text.replace(/\\u[\dA-F]{4}/gi, function(match) {
+      return String.fromCharCode(parseInt(match.replace(/\\u/g, ""), 16));
+    });
+  };
+
+  useEffect(() => {
+    setDecodeValue(unicodeToChar(value));
+  }, [value]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Input multiline={true} value={value} onChange={e => setValue(e.target.value)} />
+      {decodeValue}
     </div>
   );
 }
